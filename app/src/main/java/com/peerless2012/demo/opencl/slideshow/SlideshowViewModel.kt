24415df7524d51a8f75e4aa810ac4543d5e9a05c
peerless2012 <peerless2012@126.com>
2023-02-14 11:42:31 +0800

    Init Android project

---
+++ app/src/main/java/com/peerless2012/demo/opencl/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.peerless2012.demo.opencl.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
